79d58cea294ef5b49343bbac6870d1e528474a89
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-11-25 20:43:32 +0100

    project test не работает условие с датами, токен работает полностью

--- bookingApps/utils/date_selection_utils.py
+++ bookingApps/utils/date_selection_utils.py
@@ -21,52 +21,44 @@ class DateSelectionUtils:
         for i in date_departure_db:
             list_departure.append(i['date_departure'])
         date_now = datetime.date.today()
-        arrival = DateSelectionModel.objects.filter(date_arrival__gt=date_arrival).filter(date_departure__gte=date_departure).exists()
-        print('arrival:',arrival)
-        if not arrival:
-            arrival1 = DateSelectionModel.objects.filter(date_arrival__lt=date_arrival).filter(date_departure__lt=date_arrival).exists()
-            # departure = DateSelectionModel.objects.filter(date_departure__lt=date_arrival).filter(date_departure__lt=date_departure).exists()
-            print('departure:', arrival1)
-            if not arrival1:
+
+        def now():
+            if date_arrival >= date_now and date_departure > date_now:
+                return True
+            else:
                 return False
-        return True
-        # def now():
-        #     if date_arrival >= date_now and date_departure > date_now:
-        #         return True, print('true_now')
-        #     else:
-        #         return False, print('false_now')
-        #
-        # def arrival():
-        #     for i in list_arrival:
-        #         print(i)
-        #         if date_arrival < i and date_departure <= i:
-        #             return True, print('true_arrival1')
-        #         else:
-        #             continue
-        #     return False, print('false_arrival1')
-        #
-        # def department():
-        #     for j in list_departure:
-        #         print(j)
-        #         if date_arrival >= j and date_departure > j:
-        #             return True, print('true_departure')
-        #         else:
-        #             continue
-        #     return False, print('false_departure')
-        #
-        # def date():
-        #     if arrival() != department():
-        #         return True, print('true_date')
-        #     else:
-        #         return False, print('false_date')
-        #
-        # def start():
-        #     if now() == date():
-        #         return True
-        #     else:
-        #         return False
-        #
-        # return start()
+
+        def arrival():
+            for i in list_arrival:
+                print(i)
+                if date_arrival < i and date_departure <= i:
+                    return True
+                else:
+                    continue
+            return False
+
+        def department():
+            for j in list_departure:
+                print(j)
+                if date_arrival >= j and date_departure > j:
+                    return True
+                else:
+                    continue
+            return False
+
+        def date():
+            if arrival() != department():
+                return True
+            else:
+                return False
+
+        def start():
+            if now() == date():
+                return True
+            else:
+                return False
+
+        return start()
 
     @classmethod
     def date_selection(cls, request: Request):
