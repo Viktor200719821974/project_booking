aef37a22853463ad6949019b76972a3538cd3864
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-12-08 21:33:13 +0100

    project test  подправил serializer comments_apartment

--- apps/comments_apartment/models.py
+++ apps/comments_apartment/models.py
@@ -11,8 +11,8 @@ class CommentsApartmentModel(models.Model):
         ordering = ('id',)
 
     comments = models.CharField(max_length=200)
-    rating = models.FloatField(max_length=3, validators=[V.MinValueValidator(1, 3), V.MaxValueValidator(5, 3)])
-    average_rating = models.FloatField(default=0)
+    rating = models.FloatField(max_length=3, validators=[V.MinValueValidator(1, 2), V.MaxValueValidator(5, 2)])
+    average_rating = models.FloatField(default=False)
     apartment = models.ForeignKey(ApartmentModel, on_delete=models.CASCADE, related_name='comments_apartment')
 
 
