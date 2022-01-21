8b0299d176a459bcd5c717553eab64cd2a55f878
Viktor200719821974 <viktorvanyatanya@gmail.com>
2022-01-21 18:37:59 +0100

    change model date selection, add surname user

--- bookingApps/utils/type_token_utils.py
+++ bookingApps/utils/type_token_utils.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 
 from bookingApps.utils.email_utils import EmailUtils
@@ -19,20 +20,25 @@ class TypeToken:
     @classmethod
     def send_email_sleep(cls, email, name: str, date_arrival: datetime, date_departure: datetime,
                          cost: float, number_days: datetime, number_peoples: int):
-        while True:
-            type = TypeToken.send_email_user()
-            print(type)
 
+        start_time = time.time()
+        CLOSE_AFTER = 300
+
+        while True:
+
+            type = TypeToken.send_email_user()
             if type == 'yes':
                 EmailUtils.lease_confirmation_tenant(email, name=name, date_arrival=date_arrival,
                                                      date_departure=date_departure, cost=cost,
                                                      number_days=number_days,
                                                      number_peoples=number_peoples)
                 break
-            elif type == 'no':
+            elif type == 'no' or time.time() > start_time + CLOSE_AFTER:
                 EmailUtils.lease_confirmation_tenant_rent_no(email, name=name, date_arrival=date_arrival,
                                                              date_departure=date_departure)
                 raise NoRentException
+
+
 
             else:
                 continue
